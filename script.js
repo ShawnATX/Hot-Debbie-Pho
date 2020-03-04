edabe185422ebf07d6bcb99c8fde4fa833bc25c5
Shawn Wright <volushawn@gmail.com>
2020-03-04 14:06:38 -0600

    reserve front end call

--- script.js
+++ script.js
@@ -20,3 +20,20 @@
         }
       });
     });
+
+
+
+
+
+
+    $("#reserve-btn").on("click", () => {
+      $("#reserve-btn").preventDefault();
+      const name = $("#form-name").val().trim();
+      const phone = $("#form-phone").val().trim();
+      const email = $("#form-email").val().trim();
+      const party = $("#form-party").val().trim();
+
+
+      
+      $.post("/reserve", data)
+    })
