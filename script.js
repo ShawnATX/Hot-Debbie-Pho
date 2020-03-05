6cae84c29f05e1273581b7288d33a78a2942ab79
Shawn Wright <volushawn@gmail.com>
2020-03-05 10:04:42 -0600

    Reservation front-end

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-    $("#search-btn").on("click", function() {
-      var searchedCharacter = $("#character-search").val().trim();
+    // $("#search-btn").on("click", function() {
+    //   var searchedCharacter = $("#character-search").val().trim();
 
-      // Using a RegEx Pattern to remove spaces from searchedCharacter
-      // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-      searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
+    //   // Using a RegEx Pattern to remove spaces from searchedCharacter
+    //   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    //   searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
 
-      $.get("/", function(data) {
-        console.log(data);
-        if (data) {
-          $("#stats").show();
-          $("#name").text(data.name);
-          $("#role").text(data.role);
-          $("#age").text(data.age);
-          $("#force-points").text(data.forcePoints);
-        } else {
-          $("#name").text(
-            "The force is not strong with this one. Your character was not found.");
-          $("#stats").hide();
-        }
-      });
-    });
+    //   $.get("/", function(data) {
+    //     console.log(data);
+    //     if (data) {
+    //       $("#stats").show();
+    //       $("#name").text(data.name);
+    //       $("#role").text(data.role);
+    //       $("#age").text(data.age);
+    //       $("#force-points").text(data.forcePoints);
+    //     } else {
+    //       $("#name").text(
+    //         "The force is not strong with this one. Your character was not found.");
+    //       $("#stats").hide();
+    //     }
+    //   });
+    // });
 
     $("#reserve-btn").on("click", () => {
       $("#reserve-btn").preventDefault();
-      const name = $("#form-name").val().trim();
-      const phone = $("#form-phone").val().trim();
-      const email = $("#form-email").val().trim();
-      const party = $("#form-party").val().trim();
+      const reservation = {
+        name: $("#reserve-name").val().trim(),
+        phone: $("#reserve-phone").val().trim(),
+        email: $("#reserve-email").val().trim(),
+        partySize: $("#reserve-party").val().trim()
 
+      }
       
-      $.post("/reserve", data)
+      $.post("/reserve", reservation)
       .then(function(data) {
-        console.log("add.html", data);
-        alert("Character added!");
+        //console.log("add.html", data);
       }).fail(function(err){
         console.log(err);
       })
